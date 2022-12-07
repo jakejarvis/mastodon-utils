1f8f30d8cfdb5a6a08d7da0295fd8f175abca7ef
Jake Jarvis <jake@jarv.is>
2022-12-07 13:12:03 -0500

    sturdier apply patches script

--- scripts/apply_patches.sh
+++ scripts/apply_patches.sh
@@ -8,14 +8,18 @@ MASTODON_ROOT=/home/mastodon
 
 if [ ! -d "$MASTODON_ROOT/scripts" ]
 then
-  sudo -u mastodon git clone https://github.com/jakejarvis/mastodon-scripts.git ./scripts
+  # clone repo
+  sudo -u mastodon git clone https://github.com/jakejarvis/mastodon-scripts.git "$MASTODON_ROOT/scripts"
+
+  # fix permissions
+  sudo chown -R mastodon:mastodon "$MASTODON_ROOT/scripts"
+  sudo git config --global --add safe.directory "$MASTODON_ROOT/scripts"
 fi
 
 # pull latest patches
 cd "$MASTODON_ROOT/scripts"
-sudo -u mastodon git fetch --all
 sudo -u mastodon git pull origin main
 
 # apply custom patches
 cd "$MASTODON_ROOT/live"
-sudo -u mastodon git apply --allow-binary-replacement --whitespace=warn "$MASTODON_ROOT"/mastodon-scripts/patches/*.patch || true
+sudo -u mastodon git apply --allow-binary-replacement --whitespace=warn "$MASTODON_ROOT"/scripts/patches/*.patch || true
